c5da5d2caf9f83a39bd2c95f7c1bfe4ccc499a30
Barsch2006 <cchr98756@gmail.com>
2023-02-24 21:38:39 +0100

    switch betweeen groups

--- index.js
+++ index.js
@@ -20,3 +20,16 @@ function load() {
         }
     });
 }
+
+function changechannelListChanger(id) {
+    document.querySelector('#channelListChanger4').classList.remove('selected')
+    document.querySelector('#channelListChanger3').classList.remove('selected')
+    document.querySelector('#channelListChanger2').classList.remove('selected')
+    document.querySelector('#channelListChanger1').classList.remove('selected')
+    document.querySelector(`#channelListChanger${id}`).classList.add('selected')
+    document.querySelector(`#channelList1`).style.display='none'
+    document.querySelector(`#channelList2`).style.display='none'
+    document.querySelector(`#channelList3`).style.display='none'
+    document.querySelector(`#channelList4`).style.display='none'
+    document.querySelector(`#channelList${id}`).style.display='flex'
+}
